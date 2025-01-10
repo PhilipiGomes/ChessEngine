1b45fb086af8619e4239512149fb1f670b30710c
PhilipiGomes <philipi.rodrigues342@gmail.com>
2025-01-10 00:03:47 -0300

    Play on Lichess (kinda buggy)

--- AiLichess.py
+++ AiLichess.py
@@ -2,7 +2,7 @@ import berserk
 import chess
 from Engine import get_best_move
 
-LICHESS_API_TOKEN = input('Insert your Lichess API token: ')
+LICHESS_API_TOKEN = 'Your_API_token'
 
 
 def ai_play_on_lichess_bot(depth):
@@ -28,41 +28,41 @@ def ai_play_on_lichess_bot(depth):
 def play_game(client, game_id, depth):
     game_stream = client.bots.stream_game_state(game_id)
 
-    board = chess.Board()  # Inicializando o tabuleiro principal
-    board2 = chess.Board() # Tabuleiro para transformação de movimentos de UCI para SAN (Por causa das aberturas)
+    board = chess.Board()  # Starting Main board
+    board2 = chess.Board() # Board to transform UCI moves in SAN moves (Because of the openings)
     white_player = None
     black_player = None
-    account_username = client.account.get()['username'].lower()  # Armazenando o nome de usuário uma vez
-    moves_san = []  # Lista para armazenar todos os movimentos em SAN
+    account_username = client.account.get()['username'].lower()  # Storing the username
+    moves_san = []  # List to store all the SAN moves
     initial_fen = chess.STARTING_FEN
 
     for event in game_stream:
         if event['type'] == 'gameFull':
-            board.set_fen(initial_fen)  # Atualiza o tabuleiro principal
-            board.set_fen(initial_fen)  # Atualiza o tabuleiro 2
+            board.set_fen(initial_fen)  # Update main board
+            board.set_fen(initial_fen)  # Update board 2
 
             white_player = event['white']['id']
             black_player = event['black']['id']
 
-            # Se for a vez do bot, faz a jogada da IA
+            # If it's bot's turn, then play
             if board.turn == chess.WHITE and account_username == white_player:
                 make_best_move(client, game_id, board, depth, moves_san)
 
         elif event['type'] == 'gameState':
             moves = event['moves'].split()
 
-            for move in moves[len(board.move_stack):]:  # Aplica apenas os novos movimentos no tabuleiro
+            for move in moves[len(board.move_stack):]:  # Apply only the last move on the board
                 board.push(chess.Move.from_uci(move))
 
             for move in moves[:-1]:
                 board2.push(chess.Move.from_uci(move))
 
-            # Adiciona os movimentos em SAN à lista moves_san (Usando o tabuleiro 2, resetando ele toda vez, para não travar na hra de executar movimentos.)
+            # Add the moves in SAN to the moves_san list (Using board 2, resetting it every time, so as not to get stuck when executing moves.)
             move_san = board2.san(chess.Move.from_uci(moves[-1]))
             moves_san.append(move_san)
-            board2.set_fen(initial_fen) # resetando de novo o tabuleiro.
+            board2.set_fen(initial_fen) # Resetting the board 2
 
-            # Fazer a jogada da IA se for a vez do bot
+            # Make the bot's play in his turn
             if (board.turn == chess.WHITE and account_username == white_player) or \
                (board.turn == chess.BLACK and account_username == black_player):
                 make_best_move(client, game_id, board, depth, moves_san)
@@ -80,11 +80,9 @@ def make_best_move(client, game_id, board, depth, moves):
     if best_move is not None:
         move_uci = best_move.uci()
         client.bots.make_move(game_id, move_uci)
-        board.push(best_move)  # Atualiza o tabuleiro principal após a jogada
+        board.push(best_move)  # Update main board after play
     else:
         print('Game Over')
-
-
 
 
 # Start AI game as a Bot on Lichess
